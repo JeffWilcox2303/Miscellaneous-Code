64b368652b71ba2a4a88b9c8d22bad13d8074062
Jeff <jtw88@cornell.edu>
2025-10-02 01:34:02 -0400

    Updated SPI code

--- Pico_DDS/Pico_DDS.c
+++ Pico_DDS/Pico_DDS.c
@@ -13,27 +13,30 @@ int main()
 {
     regs = &DAC_data;
     stdio_init_all();
-    gpio_init(LED);
+    
     gpio_set_function(18, GPIO_FUNC_SPI);
     gpio_set_function(19, GPIO_FUNC_SPI);
-    gpio_set_function(17, GPIO_FUNC_SPI) ;
+    gpio_set_function(17, GPIO_FUNC_SPI);
+
+    gpio_init(LED);
     gpio_set_dir(LED,true);
     
     gpio_put(LED,light);
 
-    spi_init(spi0, 20000);
-    spi_set_format(spi0,16,SPI_CPOL_0,SPI_CPHA_0,SPI_LSB_FIRST);
+    spi_init(spi0, 48000);
+    spi_set_format(spi0,16,SPI_CPOL_0,SPI_CPHA_0,SPI_MSB_FIRST);
 
     while (true) {
         gpio_put(LED,light);
         light = !light;
-        // data_0 = data_0 << 1;
-        // if (data_0 == 0)
-        // {
-        //     data_0 = 1;
-        // }
+        data = data << 1;
+        if (data == 0)
+        {
+            data = 1;
+        }
         
-        DAC_data = configs && 0xF000 || data && 0x0FFF;
-        spi_write16_blocking(spi0,regs,16);
+        DAC_data = (configs & 0xF000) | (data & 0x0FFF);
+        spi_write16_blocking(spi0,regs,1);
+        // sleep_ms(100);
     }
 }
